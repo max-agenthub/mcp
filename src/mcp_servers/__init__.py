1b3ef37d35f5ca6153e4a4930ff518384c4ef0a9
MaxBrodeurUrbas <max@agenthub.dev>
2025-03-11 23:05:12 -0700

    Update __init__.py

--- src/mcp_servers/__init__.py
+++ src/mcp_servers/__init__.py
@@ -5,7 +5,6 @@ This package provides various MCP server implementations.
 
 from .base import BaseMCPServer, MCPServerRegistry, ServerInfo, ServerType
 from .slack import slack_server
-from .googlesheets import googlesheets_server
 
 __all__ = [
     "BaseMCPServer",
@@ -13,5 +12,4 @@ __all__ = [
     "ServerInfo",
     "ServerType",
     "slack_server",
-    "googlesheets_server",
 ] 
